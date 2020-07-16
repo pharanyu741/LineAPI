3919b7930b51a6a2436fbf347c67b1bf271da604
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-16 16:04:30 +0700

    make it better

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const request = require('request')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
 
+
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
