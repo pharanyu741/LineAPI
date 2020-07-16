b2db74121a51898e79b71d4b96c2bf3f5f87a456
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-16 15:57:37 +0700

    make it better

--- app.js
+++ app.js
@@ -4,11 +4,5 @@ const request = require('request')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
 
-// app.post('/webhook', (req, res) => res.sendStatus(200))
-// app.listen(port)
-
-app()
-    .use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: false}))
-    .get('/', (req, res) => res.send(`Hi there! This is a nodejs-line-api running on PORT: ${ PORT }`))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.post('/webhook', (req, res) => res.sendStatus(200))
+app.listen(port)
