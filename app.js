b838b0869c6a4d8001e3329ea5425e3da3fe943f
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-16 16:08:40 +0700

    make it better

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 4000
 
 
-app.post('/webhook', (req, res) => res.sendStatus(200))
-app.listen(port)
+app.post('/webhook', (req, res) => {
+    let replyToken = req.body.events[0].replyToken;
+    let msg = req.body.events[0].message.text;
+        
+    console.log(`Message token : ${ replyToken }`);
+    console.log(`Message from chat : ${ msg }`);
+
+    res.json({
+        status: 200,
+        message: `Webhook is working!`
+    });
+})
 
 app.get('/', (req, res) => {
     res.send("hello world")
 })
+
+// PORT
+app.listen(port)
