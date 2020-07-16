97cf7d3b5cefe7a5f83821aca3fd967d421d0305
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-16 20:49:44 +0700

    make it better

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const request = require('request')
+const axios = require('axios')
 const port = process.env.PORT || 4000
 const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message/reply';
 const LINE_HEADER = {
@@ -9,24 +10,28 @@ const LINE_HEADER = {
     'Authorization': 'Bearer {jhfNrA/i4anorqWcZXFexfXBzehsU8srKgjTTXPVPQvlAjDk5/JBehz+fLidbgRGZ4nUZxUIxGY0lu5SvUKnZBxLRPCX1iiwm7Q4OzWNREUUWWyBkpXMG3f0knk5VrDJIGWYhrHu/GeuuWUAFTvTawdB04t89/1O/w1cDnyilFU=}'
 };
 
-const fixer = require("fixer-api");
-fixer.set({accessKey: "0ce347832d173f2f35790ef8ae0b527f"})
-
-
-
+/*/*/
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
     reply(reply_token, msg)
+    exchangeRate(msg)
+
     res.sendStatus(200)
 })
 
 /* FUNCTIONS */
-function reply(reply_token, msg) {
-    let data = fixer.latest({ base: "USD", symbols: ["CHF"] });
-    console.log(data);
+function exchangeRate(msg) {
+    axios.get('http://data.fixer.io/api/latest?access_key=0ce347832d173f2f35790ef8ae0b527f&format=1')
+    .then(response => {
+        let result = response.data.rate[USD]
+        let rate = result * msg
+        console.log(rate);
+    })
+}
 
+function reply(reply_token, msg) {
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
