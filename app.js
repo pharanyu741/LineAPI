2d9bfbc29ff9e4410369de13ed897b143aee1d6c
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-16 19:44:59 +0700

    make it better

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const request = require('request')
-const app = express()
 const port = process.env.PORT || 4000
 const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message/reply';
 const LINE_HEADER = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer {jhfNrA/i4anorqWcZXFexfXBzehsU8srKgjTTXPVPQvlAjDk5/JBehz+fLidbgRGZ4nUZxUIxGY0lu5SvUKnZBxLRPCX1iiwm7Q4OzWNREUUWWyBkpXMG3f0knk5VrDJIGWYhrHu/GeuuWUAFTvTawdB04t89/1O/w1cDnyilFU=}'
-  };
+};
 
-// app.use(bodyParser.urlencoded({
-//      extended: false 
-//     }))
+const fixer = require("fixer-api");
+fixer.set({accessKey: "0ce347832d173f2f35790ef8ae0b527f"})
+
 app.use(bodyParser.json())
-
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
     reply(reply_token, msg)
     res.sendStatus(200)
+    const data = await fixer.latest({ base: "USD", symbols: ["CHF"] });
+    console.log(data);
+
 })
 
+/* FUNCTIONS */
 function reply(reply_token, msg) {
     let body = JSON.stringify({
         replyToken: reply_token,
