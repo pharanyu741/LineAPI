e17739070b912e5d7d6b69b7d28457526d88baef
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-16 21:01:54 +0700

    make it better

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ app.post('/webhook', (req, res) => {
 function exchangeRate(msg) {
     axios.get('http://data.fixer.io/api/latest?access_key=0ce347832d173f2f35790ef8ae0b527f&format=1')
     .then(response => {
-        let result = response.data.rate[USD]
-        let rate = result * msg
-        console.log(rate);
+        // let result = response.data.rate[USD]
+        // let rate = result * msg
+        console.log(response.body);
     })
     .catch(error => {
         console.log(error);
