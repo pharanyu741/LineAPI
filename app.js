03bcaa73a9b84c88395c1851737bf6767759783f
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-17 11:47:37 +0700

    make it better

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const LINE_HEADER = {
 /*/*/
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
-    let event = req.body.event[0]
+    // let event = req.body.event[0]
     // switch (event.type) {
     //     case 'message':
     //         if(event.message.type === 'text') {
@@ -25,11 +25,12 @@ app.post('/webhook', (req, res) => {
             
     //     break;
     // }
-    // let token = req.body.events[0].replyToken
-    // let msg = req.body.events[0].message.text
-    let token = event.replyToken
-    let msg = event.message.text
-    // exchangeRate(msg)
+    
+    // let token = event.replyToken
+    // let msg = event.message.text
+    let token = req.body.events[0].replyToken
+    let msg = req.body.events[0].message.text
+    exchangeRate(msg)
     reply(token, msg)
     res.sendStatus(200)
 })
