2f68b7ea6909aa73da9b9ff214f720586a12f2aa
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-17 14:49:02 +0700

    make it better

--- app.js
+++ app.js
@@ -16,31 +16,32 @@ app.post('/webhook', (req, res) => {
     let event = req.body.events[0];
     let token = event.replyToken
     let msg = event.message.text
+    splitStr(token, msg)
+    res.sendStatus(200)
+})
+
+/* FUNCTIONS */
+function splitStr(token, msg) {
+    let newMs = msg.split(" ")
     if(event.message.type === 'text') {
-        if(!isNaN(msg)) {
-            exchangeRate(token, msg)
+        let num = newMs[0]
+        let Currency = newMs[1]
+        if(!isNaN(num)) {
+            exchangeRate(token, num, Currency)
         }else{
             let res = "กรุณาระบุ จำนวนเงินเป็นตัวเลขเท่านั้น"
             reply(token, res)
         }
     }
-    // console.log(newMsg);
-    splitStr(msg)
-    res.sendStatus(200)
-})
 
-/* FUNCTIONS */
-function splitStr(msg) {
-    let newMs = msg.split(" ")
-    console.log(newMs); 
 }
 
-function exchangeRate(token, msg) {
+function exchangeRate(token, num, Currency) {
     axios.get('http://data.fixer.io/api/latest?access_key=0ce347832d173f2f35790ef8ae0b527f&format=1')
     .then(response => {
-        let euroBase = 1/response.data.rates['USD']
+        let euroBase = 1/response.data.rates[Currency]
         let rate = euroBase * response.data.rates['THB']
-        let sum = rate*msg
+        let sum = rate*num
         let res = "เป็นเงิน "+sum.toFixed(2)+" บาท";
         reply(token, res)
     })
