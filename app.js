9602907ec5cd946c4273d871dda3a219aefcbd2e
pharanyu741 <pharanyu.wari@bumail.net>
2020-07-17 16:53:07 +0700

    make it better

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function exchangeRate(token, msg) {
         let newMs = msg.split(" ")
         if(newMs[0]&&newMs[1]&&!newMs[2]) {
             let num = newMs[0]
-            if(!isNan(num)) {
+            if(isNan(num) === false) {
                 let currency = newMs[1].toUpperCase()
                 let euroBase = 1/response.data.rates[currency]
                 if(euroBase) {
